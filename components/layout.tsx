2496fcfd718aeeb28e07f48387003461077f63c7
John Oerter <john@johnoerter.me>
2020-11-18 20:27:45 -0600

    Update optional props

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,18 +6,12 @@ export const siteTitle = 'Red Green Refactor';
 const defaultImage = 'https://redgreenrefactor.dev/images/logo-cropped.png'
 const defaultDescription = 'Red Green Refactor is the personal blog site for John Oerter, a software developer in Omaha, NE'
 
-export default function Layout({
-    children,
-    home,
-    metaImage,
-    metaTitle,
-    metaDescription
-}: {
+export default function Layout(props : {
     children: React.ReactNode;
     home?: boolean;
-    metaImage: string;
-    metaTitle: string;
-    metaDescription: string;
+    metaImage?: string;
+    metaTitle?: string;
+    metaDescription?: string;
 }) {
     return (
         <div className={styles.container}>
@@ -25,17 +19,17 @@ export default function Layout({
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content={metaDescription || defaultDescription}
+                    content={props.metaDescription || defaultDescription}
                 />
                 <meta
                     name="og:description"
-                    content={metaDescription || defaultDescription}
+                    content={props.metaDescription || defaultDescription}
                 />
                 <meta
                     property="og:image"
-                    content={metaImage || defaultImage}
+                    content={props.metaImage || defaultImage}
                 />
-                <meta name="og:title" content={metaTitle || siteTitle} />
+                <meta name="og:title" content={props.metaTitle || siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
             <header className={styles.header}>
@@ -51,8 +45,8 @@ export default function Layout({
                     </Link>
                 </nav>
             </header>
-            <main>{children}</main>
-            {!home && (
+            <main>{props.children}</main>
+            {!props.home && (
                 <div className={styles.backToHome}>
                     <Link href="/">
                         <a>← Back to home</a>
