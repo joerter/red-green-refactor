1846cd2a9351a433fc01044f0946977769d34423
John Oerter <john@johnoerter.me>
2020-12-08 22:13:02 -0600

    use effect on newsletter

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -1,14 +1,23 @@
 import { newsletterForm } from '../lib/convert-kit';
+import { useEffect } from 'react';
 
 export default function Newsletter() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://unique-creator-4815.ck.page/6b6b63168f/index.js';
+        script.async = true;
+        script.setAttribute('data-uid', '6b6b63168f');
+
+        const mountPoint = document.getElementById('signup');
+        mountPoint.appendChild(script);
+    });
+
     return (
         <div>
             <h2>
                 Want to be notified about my next post? Join the newsletter!
             </h2>
-            <div>
-                <div dangerouslySetInnerHTML={{ __html: newsletterForm }}></div>
-            </div>
+            <div id="signup"></div>
         </div>
     );
 }
--- lib/convert-kit.ts
+++
@@ -1,1 +0,0 @@
-export const newsletterForm = `<script async data-uid="6b6b63168f" src="https://unique-creator-4815.ck.page/6b6b63168f/index.js"></script>`;
