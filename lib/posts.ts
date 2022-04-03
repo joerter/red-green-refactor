6a9c4c86cdd0558f362d1635ff90671253d0543d
John Oerter <john.oerter@passportinc.com>
2022-04-03 13:59:40 -0400

    Start new post and fix code highlighting

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import { remark } from 'remark';
-import html from 'remark-html';
+import {unified} from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeStringify from 'rehype-stringify'
 
 export interface PostData {
     date: string;
@@ -19,7 +22,8 @@ export function getSortedPostsData(): PostData[] {
     const fileNames = fs.readdirSync(postsDirectory);
     const allPostsData = fileNames.map((fileName) => {
         // Remove ".md" from file name to get id
-        const id = fileName.replace(/\.md$/, '');
+        const id = fileName.replace(/\.md$/,
+'');
 
         // Read markdown file as string
         const fullPath = path.join(postsDirectory, fileName);
@@ -91,9 +95,11 @@ export async function getPostData(id: string) {
     const matterResult = matter(fileContents);
     const tags = matterResult.data.tags;
 
-    // Use remark to convert markdown into HTML string
-    const processedContent = await remark()
-        .use(html)
+    const processedContent = await unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeHighlight)
+        .use(rehypeStringify)
         .process(matterResult.content);
     const contentHtml = processedContent.toString();
 
