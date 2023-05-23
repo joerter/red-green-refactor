2837eacb95c1dacdb82636facac7ed1c26379807
John Oerter <john@johnoerter.me>
2023-05-22 22:52:27 -0400

    Parse and display posts content

--- app/theme/themeOptions.ts
+++ app/theme/themeOptions.ts
@@ -1,14 +1,30 @@
 import { ThemeOptions } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+  interface Palette {
+    gradient: Palette["background"];
+  }
+
+  interface PaletteOptions {
+    gradient: Palette["background"];
+  }
+}
+
 export const themeOptions: ThemeOptions = {
   palette: {
     mode: "light",
     primary: {
       main: "#21295C",
-      //main: "#488EE3",
     },
     secondary: {
       main: "#8db905",
+    },
+    info: {
+      main: "#488EE3"
+    },
+    gradient: {
+      paper: "linear-gradient(to right, #21295C, #5c69be)",
+      default: "linear-gradient(to right, #21295C, #5c69be)",
     },
   },
   typography: {
