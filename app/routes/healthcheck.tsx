f19e3f0df733ddbf8bcd66e0c3e9de1d2e33602f
John Oerter <john@johnoerter.me>
2023-05-27 20:07:09 -0400

    Simplifiy healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,7 +1,7 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderArgs } from "@remix-run/node";
 
-import { prisma } from "~/db.server";
+// import { prisma } from "~/db.server";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const host =
@@ -12,7 +12,7 @@ export const loader = async ({ request }: LoaderArgs) => {
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
-      prisma.user.count(),
+      // prisma.user.count(),
       fetch(url.toString(), { method: "HEAD" }).then((r) => {
         if (!r.ok) return Promise.reject(r);
       }),
